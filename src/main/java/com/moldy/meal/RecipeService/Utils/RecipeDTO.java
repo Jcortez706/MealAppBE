576bffcbb2812395e596a811aabb24435fffed94
Jacob Cortez <jcortez706@aol.com>
2024-11-09 12:54:26 -0500

    [MA-9]: Create Recipe DTO

---
+++ src/main/java/com/moldy/meal/RecipeService/Utils/RecipeDTO.java
@@ -0,0 +1,8 @@
+package com.moldy.meal.RecipeService.Utils;
+
+public class RecipeDTO {
+    private int recipeID;
+    private String instructions;
+    private String ingredient;
+    private String recipeName;
+}
