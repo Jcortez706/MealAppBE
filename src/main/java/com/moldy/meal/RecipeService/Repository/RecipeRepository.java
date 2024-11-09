b44c70748f2dd11ab5705623fa85085bb83e8abe
Jacob Cortez <jcortez706@aol.com>
2024-11-09 16:43:20 -0500

    [MA-9]: getByID jpa query

--- src/main/java/com/moldy/meal/RecipeService/Repository/RecipeRepository.java
+++ src/main/java/com/moldy/meal/RecipeService/Repository/RecipeRepository.java
@@ -1,11 +1,15 @@
-package com.moldy.meal.RecipeService;
+package com.moldy.meal.RecipeService.Repository;
 
+import com.moldy.meal.RecipeService.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     @Query("SELECT r.recipeID FROM Recipe r")
     List<Integer> findAllRecipeID();
+    @Query("SELECT r FROM Recipe r WHERE r.recipeID = :id")
+    Recipe getByID(@Param("id")Integer id);
 }
