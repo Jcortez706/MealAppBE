7ad7d53125b114f6bf921a4b28057b6437c06417
Jacob Cortez <jcortez706@aol.com>
2024-11-10 22:21:09 -0500

    [MA-9]: updated get method to return a ResponseDTO

--- src/main/java/com/moldy/meal/RecipeService/Controller/RecipeController.java
+++ src/main/java/com/moldy/meal/RecipeService/Controller/RecipeController.java
@@ -2,8 +2,10 @@ package com.moldy.meal.RecipeService.Controller;
 
 import com.moldy.meal.RecipeService.Recipe;
 import com.moldy.meal.RecipeService.Repository.RecipeRepository;
+import com.moldy.meal.RecipeService.Utils.DTO.ResponseRecipeDTO;
 import com.moldy.meal.RecipeService.Utils.Mapping.ConvertRecipeDTOtoRecipe;
 import com.moldy.meal.RecipeService.Utils.DTO.RequestRecipeDTO;
+import com.moldy.meal.RecipeService.Utils.Mapping.ConvertResponseRecipeDTOToRecipe;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +18,13 @@ public class RecipeController {
     RecipeRepository recipeRepository;
 
     @GetMapping("/{id}")
-    ResponseEntity<Recipe> getRecipe(@PathVariable("id") Integer id){
+    ResponseEntity<ResponseRecipeDTO> getRecipe(@PathVariable("id") Integer id) {
         Recipe recipe = recipeRepository.getByID(id);
-        return ResponseEntity.ok(recipe);
+        ResponseRecipeDTO response = new ConvertResponseRecipeDTOToRecipe(recipe).getConvertedRecipeDTO();
+        return ResponseEntity.ok(response);
     }
     @PostMapping
-    ResponseEntity<String> postRecipe(@Valid @RequestBody RequestRecipeDTO recipe){
+    ResponseEntity<String> postRecipe(@Valid @RequestBody RequestRecipeDTO recipe) {
         Recipe recipePost = new ConvertRecipeDTOtoRecipe(recipe).getConvertedRecipe();
         recipeRepository.save(recipePost);
         return ResponseEntity.ok("valid");
