c975efa88a87c3d651df6acd41eeacc5cb44b1d7
Jacob Cortez <jcortez706@aol.com>
2024-11-10 22:44:07 -0500

    [MA-9]: added get method unit test

--- src/test/java/com/moldy/meal/RecipeService/Controller/RecipeControllerTest.java
+++ src/test/java/com/moldy/meal/RecipeService/Controller/RecipeControllerTest.java
@@ -5,6 +5,8 @@ import com.moldy.meal.RecipeService.Recipe;
 import com.moldy.meal.RecipeService.Repository.RecipeRepository;
 import com.moldy.meal.RecipeService.Utils.DTO.RequestRecipeDTO;
 import com.moldy.meal.RecipeService.Utils.DTO.RequestRecipeDTOmock;
+import com.moldy.meal.RecipeService.Utils.DTO.ResponseRecipeDTO;
+import com.moldy.meal.RecipeService.Utils.DTO.ResponseRecipeDTOMock;
 import com.moldy.meal.RecipeService.Utils.Mapping.ConvertRecipeDTOtoRecipe;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,19 +35,37 @@ class RecipeControllerTest {
 
     @Test
     void testPostRecipe() throws Exception {
-        // Prepare test data
         RequestRecipeDTO mockRecipe = RequestRecipeDTOmock.createMock();
 
         Recipe recipe = new ConvertRecipeDTOtoRecipe(mockRecipe).getConvertedRecipe();
 
-        // Mock the repository save operation
         Mockito.when(recipeRepository.save(Mockito.any(Recipe.class))).thenReturn(recipe);
 
-        // Perform POST request
         mockMvc.perform(post("/recipe")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(mockRecipe)))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("valid"));
+    }
+
+    @Test
+    void testGetRecipe() throws Exception {
+        Recipe mockRecipe = new Recipe();
+        mockRecipe.setRecipeID(1);
+        mockRecipe.setRecipeName("Classic Cake Recipe");
+        mockRecipe.setIngredient("Flour, Sugar, Eggs, Butter");
+        mockRecipe.setInstructions("Mix all ingredients thoroughly and bake at 350 degrees for 25 minutes.");
+
+        ResponseRecipeDTO mockResponse = ResponseRecipeDTOMock.createMock();
+
+        Mockito.when(recipeRepository.getByID(Mockito.anyInt())).thenReturn(mockRecipe);
+
+        String expectedResponseJson = objectMapper.writeValueAsString(mockResponse);
+
+        mockMvc.perform(get("/recipe/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockRecipe)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(expectedResponseJson));
     }
 }
